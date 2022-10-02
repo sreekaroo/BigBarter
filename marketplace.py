3a4fcaa9132c906019a2b13b902dc3f1a8551567
adityavc <adityavc@umich.edu>
2022-10-02 10:36:53 -0400

    revert

--- marketplace.py
+++ marketplace.py
@@ -1,25 +1,37 @@
+
 import random
 from turtle import onclick
 import streamlit as st
 import pandas as pd
 import numpy as np
 import uuid
-<<<<<<< HEAD
-=======
 
 people = [['Aditya', 'banan'], ['Ad', 'apple']]
 
->>>>>>> 97655155d0b4f3127204b68f3a9f7540dfbdddc5
 
+people = []
 def populate_people():
+    st.header('People')
+    #list of people in community + items willing to barter
+    df = pd.DataFrame(
+    people,
+    columns=('col %d' % i for i in range(2)))
     st.header("People")
     # list of people in community + items willing to barter
-    df = pd.DataFrame(people, columns=['Seller', 'Commodity Selling'])
+    df = pd.DataFrame(people, columns=["col %d" % i for i in range(2)])
     st.table(df)
-
 
 def handle_registration():
     with st.sidebar:
+        st.subheader('Registration')
+        reg_form=st.form(key='reg')
+        fname=reg_form.text_input('First Name:')
+        lname=reg_form.text_input('Last Name:')
+        email=reg_form.text_input('Email:')
+        community=reg_form.text_input('Community:')
+        items=reg_form.text_input('Pick one item')
+        quantity=reg_form.slider('Pick how many',0,25)
+        address=reg_form.text_area('Enter Address:')
         st.subheader("Registration")
         reg_form = st.form(key="reg")
         fname = reg_form.text_input("First Name:")
@@ -30,11 +42,12 @@ def handle_registration():
         quantity = reg_form.slider("Pick how many", 0, 25)
         address = reg_form.text_area("Enter Address:")
         # creating a submit button
+        submit=reg_form.form_submit_button('Submit')
+        st.session_state['first_name'] = fname
+        st.session_state['last_name'] = lname
+        st.session_state['item'] = items
+        st.session_state['quantity'] = quantity
         submit = reg_form.form_submit_button("Submit")
-<<<<<<< HEAD
-    if(submit):
-        people.append([str(uuid.uuid4()), 'Apple'])
-=======
         st.session_state["first_name"] = fname
         st.session_state["last_name"] = lname
         st.session_state["item"] = items
@@ -44,68 +57,61 @@ def handle_registration():
         people.append([st.session_state['first_name'],st.session_state['last_name']])
         print(people)
         st.session_state['people'] = people
->>>>>>> 97655155d0b4f3127204b68f3a9f7540dfbdddc5
         populate_people()
 
 
 def main():
+    st.write('# BigBarter MarketPlace')  #st.title('Avocado Prices dashboard')
+    st.markdown('''
     st.write("# BigBarter MarketPlace")  # st.title('Avocado Prices dashboard')
     st.markdown(
         """
     This is a decentralized item-to-item bartering system within your custom community.\n
     First register to get started!
-
-    """
-    )
+    ''')
 
     request = st.button("Make Request")
-<<<<<<< HEAD
-    community = st.button("Join Community")
-=======
->>>>>>> 97655155d0b4f3127204b68f3a9f7540dfbdddc5
 
     if request:
+        st.subheader('Requests')
+        #display all requests with approve and reject button
+        #if approved, add transaction to transactions table
+        #update quantities of commodities
+        req_form=st.form(key='req')
+        fname=req_form.text_input('First Name:')
+        lname=req_form.text_input('Last Name:')
+        quantity=req_form.text_input('Quantity:')
+        item=req_form.text_input('Item:')
         st.subheader("Requests")
         # display all requests with approve and reject button
         # if approved, add transaction to transactions table
         # update quantities of commodities
         req_form = st.form(key="req")
-<<<<<<< HEAD
-        fname = req_form.text_input("Seller Id:")
-=======
         fname = req_form.text_input("First Name:")
         lname = req_form.text_input("Last Name:")
->>>>>>> 97655155d0b4f3127204b68f3a9f7540dfbdddc5
         quantity = req_form.text_input("Quantity:")
         item = req_form.text_input("Item:")
         # creating a submit button
+        submit=req_form.form_submit_button('Submit')
         submit = req_form.form_submit_button("Submit")
-<<<<<<< HEAD
-
-        # send request information
-        request = True
-    
-    if community:
-        st.subheader('Community')
-        com_form = st.form(key='com')
-        com = com_form.text_input('Community:')
-        submit = com_form.form_submit_button("Submit")
-        community = True
-=======
->>>>>>> 97655155d0b4f3127204b68f3a9f7540dfbdddc5
 
         # send request information
         request = True
 
-<<<<<<< HEAD
-=======
+        #send request information
+        request = False
     populate_people()
->>>>>>> 97655155d0b4f3127204b68f3a9f7540dfbdddc5
-    option = st.sidebar.selectbox(
-        "Where next?", ("home", "registration", "transactions", "requests")
-    )
+    option = st.sidebar.selectbox('Where next?', ('home','registration','transactions','requests'))
+    if option == 'home':
+        option = st.sidebar.selectbox(
+            "Where next?", ("home", "registration", "transactions", "requests")
+        )
     if option == "home":
         with st.sidebar:
+            st.subheader('Home')
+            st.write('Welcome to BigBarter Marketplace! Feel free to make an account with your current inventory' + 
+                    'and begin bartering with people in your community')
+    elif option == 'transactions':
             st.subheader("Home")
             st.write(
                 "Welcome to BigBarter Marketplace! Feel free to make an account with your current inventory"
@@ -113,6 +119,10 @@ def main():
             )
     elif option == "transactions":
         with st.sidebar:
+            st.subheader('Previous Transactions')
+            df = pd.DataFrame(
+            np.random.randn(10, 5),
+            columns=('col %d' % i for i in range(5)))
             st.subheader("Previous Transactions")
 
             num_rows = 100
@@ -139,14 +149,14 @@ def main():
 
             )
             st.table(df)
-<<<<<<< HEAD
-=======
-
->>>>>>> 97655155d0b4f3127204b68f3a9f7540dfbdddc5
     elif option == "registration":
         handle_registration()
     elif option == "requests":
         with st.sidebar:
+            st.subheader('Requests')
+            #display all requests with approve and reject button
+            #if approved, add transaction to transactions table
+            #update quantities of commodities
             st.subheader("Requests")
             # display all requests with approve and reject button
             # if approved, add transaction to transactions table
@@ -161,10 +171,6 @@ def main():
                 'Commodity Requesting Amount': 10
             }, index=[0])
             st.table(df)
-<<<<<<< HEAD
-=======
-
->>>>>>> 97655155d0b4f3127204b68f3a9f7540dfbdddc5
             col1, col2 = st.sidebar.columns([1, 1])
 
             with col1:
@@ -175,17 +181,10 @@ def main():
                 st.button("Reject", kwargs={
                     'clicked_button_ix': 2, 'n_buttons': 2
                 })
-<<<<<<< HEAD
-            
-=======
->>>>>>> 97655155d0b4f3127204b68f3a9f7540dfbdddc5
 
 
+    #store Name (first/last), items
 if __name__ == "__main__":
     main()
 
-<<<<<<< HEAD
     # store Name (first/last), items
-=======
-    # store Name (first/last), items
->>>>>>> 97655155d0b4f3127204b68f3a9f7540dfbdddc5
